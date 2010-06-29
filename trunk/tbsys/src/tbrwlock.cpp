2f37a57d0ed3388f574cceb7912b6ee212452c98
maoqi@taobao.com <maoqi@taobao.com@e31be89f-a2c2-4b94-b16c-7e29e68f0872>
2010-06-29 03:46:22 +0000

    modify \r\n -> \r

--- trunk/tbsys/src/tbrwlock.cpp
+++ trunk/tbsys/src/tbrwlock.cpp
@@ -17,89 +17,88 @@
 
 using namespace tbsys;
 
-int CRLock::lock() const
+int CRLock::lock()
 {
-	return pthread_rwlock_rdlock(m_rlock);
+    return pthread_rwlock_rdlock(m_rlock);
 }
 
-int CRLock::tryLock() const
+int CRLock::tryLock()
 {
     return pthread_rwlock_tryrdlock(m_rlock) ;
 }
 
-int CRLock::unlock() const
+int CRLock::unlock()
 {
-	return pthread_rwlock_unlock(m_rlock);
+    return pthread_rwlock_unlock(m_rlock);
 }
 
-int CWLock::lock() const
+int CWLock::lock()
 {
-	return pthread_rwlock_wrlock(m_wlock) ;
+    return pthread_rwlock_wrlock(m_wlock) ;
 }
 
-int CWLock::tryLock() const
+int CWLock::tryLock()
 {
-	return pthread_rwlock_trywrlock(m_wlock);
+    return pthread_rwlock_trywrlock(m_wlock);
 }
 
-int CWLock::unlock() const
+int CWLock::unlock()
 {
-	return pthread_rwlock_unlock(m_wlock);
+    return pthread_rwlock_unlock(m_wlock);
 }
 
-//////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////
 CRWLock::CRWLock()
 {
-	pthread_rwlockattr_t  attr;
-	pthread_rwlockattr_init(&attr);
-	pthread_rwlock_init(&m_rwlock, &attr);
-	
-	m_rlock = new CRLock(&m_rwlock);
-	m_wlock = new CWLock(&m_rwlock);
+    pthread_rwlockattr_t attr;
+    pthread_rwlockattr_init(&attr);
+    pthread_rwlock_init(&m_rwlock, &attr);
+    
+    m_rlock = new CRLock(&m_rwlock);
+    m_wlock = new CWLock(&m_rwlock);
 }
 
 CRWLock::~CRWLock()
 {
     pthread_rwlock_destroy(&m_rwlock);
-	delete m_rlock;
-	delete m_wlock;
+    delete m_rlock;
+    delete m_wlock;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////
 CRWSimpleLock::CRWSimpleLock()
 {
-	pthread_rwlockattr_t  attr;
-	pthread_rwlockattr_init(&attr);
-	pthread_rwlock_init(&m_rwlock, &attr);
+    pthread_rwlockattr_t attr;
+    pthread_rwlockattr_init(&attr);
+    pthread_rwlock_init(&m_rwlock, &attr);
 }
 
 CRWSimpleLock::~CRWSimpleLock()
 {
-	pthread_rwlock_destroy(&m_rwlock);
+    pthread_rwlock_destroy(&m_rwlock);
 }
 
 int CRWSimpleLock::rdlock()
 {
-	return pthread_rwlock_rdlock(&m_rwlock);
+    return pthread_rwlock_rdlock(&m_rwlock);
 }
+
 int CRWSimpleLock::wrlock()
 {
-	 return pthread_rwlock_wrlock(&m_rwlock);
+     return pthread_rwlock_wrlock(&m_rwlock);
 }
 
 int CRWSimpleLock::tryrdlock()
 {
-	return pthread_rwlock_tryrdlock(&m_rwlock);
+    return pthread_rwlock_tryrdlock(&m_rwlock);
 }
 
 int CRWSimpleLock::trywrlock()
 {
-	return pthread_rwlock_trywrlock(&m_rwlock);
+    return pthread_rwlock_trywrlock(&m_rwlock);
 }
 
 int CRWSimpleLock::unlock()
 {
-	return pthread_rwlock_unlock(&m_rwlock);
+    return pthread_rwlock_unlock(&m_rwlock);
 }
